78bf3dac4a5c70507392553d7283ad8f28695fca
Michael Perez Acebey <walpepe35@gmail.com>
2023-05-02 15:03:08 -0400

    default port

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ var cors = require('cors');
 class Server {
     constructor () {
         this.app = express()
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios'
         //Midlewares
         this.midlewares();
